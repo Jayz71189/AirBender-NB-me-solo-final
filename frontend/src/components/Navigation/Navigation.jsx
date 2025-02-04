64246bc623671318caf9e0fdb78871d6c1041789
Jason <jayz71189@gmail.com>
2025-02-04 17:21:54 -0500

    final commit

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,31 +1,54 @@
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
+import { useModal } from "../../context/Modal";
+import LoginFormModal from "../LoginFormModal/LoginFormModal";
+import SignupFormModal from "../SignupFormModal/SignupFormModal";
 import "./Navigation.css";
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
 
+  const { setModalContent } = useModal();
+
   return (
     <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      {sessionUser && (
+      {/* Logged-out View */}
+      {!sessionUser ? (
+        <div>
+          <li onClick={() => setModalContent(<LoginFormModal />)}>
+            {" "}
+            &nbsp; Log In
+          </li>
+          <li onClick={() => setModalContent(<SignupFormModal />)}>
+            {" "}
+            &nbsp; Sign Up
+          </li>
+        </div>
+      ) : (
         <>
-          <li>
-            <NavLink to="/spots/new">Create a New Spot</NavLink>
-          </li>
-          <li>
-            <NavLink to="/spots/manage">Manage Spots</NavLink>
-          </li>
-        </>
-      )}
+          {/* Logged-in View */}
 
-      {isLoaded && (
-        <li>
-          <ProfileButton user={sessionUser} />
-        </li>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          {sessionUser && (
+            <>
+              <li>
+                <NavLink to="/spots/new">Create a New Spot</NavLink>
+              </li>
+              <li>
+                <NavLink to="/spots/manage">Manage Spots</NavLink>
+              </li>
+            </>
+          )}
+          {/* Profile Button */}
+          {isLoaded && (
+            <li>
+              <ProfileButton user={sessionUser} />
+            </li>
+          )}
+        </>
       )}
     </ul>
   );
